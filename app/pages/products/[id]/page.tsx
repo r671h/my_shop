f406c4138ee5c84db1f11c49e608d0e966133a3c
Ruslan <sergeikashtan1337@gmail.com>
2026-03-19 16:04:17 +0100

    fixes

--- app/pages/products/[id]/page.tsx
+++ app/pages/products/[id]/page.tsx
@@ -4,17 +4,15 @@ import { getProduct } from "@/app/src/api/api";
 import ProductDetails from "@/app/src/components/Product/ProductDetails/ProductDetails";
 import { useProduct } from "@/app/src/hooks/useProduct";
 import axios from "axios";
+import { use } from "react";
 
 type Props = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default function ProductPage({ params }: Props) {
-  console.log("params:", params); 
-  console.log("id:", params.id); 
-  const {product,loading,error} = useProduct(Number(params.id))
+  const { id } =  use(params); 
+  const {product,loading,error} = useProduct(Number(id))
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
