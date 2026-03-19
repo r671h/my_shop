6abc0dfbef0f3ac0e0fc0661918be1a51a589382
Ruslan <sergeikashtan1337@gmail.com>
2026-03-19 14:39:05 +0100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,11 @@ import styles from "./page.module.scss";
 import ProductList from "@/app/src/components/Product/ProductList/ProductList";
 
 export default async function Home() {
-  const products = await getProducts();
-
-  if(!products){
-    console.log("error fetching products");
-  }
-  
 
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>My Online Shop</h1>
-      <ProductList products={products} />
+      <ProductList/>
     </main>
   );
 }
--- app/src/components/Product/ProductList/ProductList.tsx
+++ app/src/components/Product/ProductList/ProductList.tsx
@@ -4,24 +4,33 @@ import { useState } from "react";
 import ProductGrid from "../ProductGrid/ProductGrid";
 import Sidebar from "../../SideBar/SideBar";
 import styles from "./ProductList.module.scss";
+import { useEffect } from "react";
+import { getProducts } from "@/app/src/api/api";
 
-type Props = {
-    products: Product[];
-}
+export default function Productlist(){
+    const [products, setProducts] = useState<Product[]>([]);
+  const [search, setSearch] = useState("");
+  const [activeCategory, setActiveCategory] = useState("all");
+  const [loading, setLoading] = useState(true);
 
-export default function Productlist({products}: Props){
-    const [search,setSearch] = useState("");
-    const [activeCategory, setActiveCategory] = useState("all");
-
-    const categories = ["all",...Array.from(new Set(products.map(p => p.category)))];
-
-    const filtered = products.filter((p) =>{
-        const matchesSearch = p.title.toLocaleLowerCase().includes(search.toLocaleLowerCase());
-        const matchesCategory = activeCategory === "all" || p.category === activeCategory;
-        return matchesSearch && matchesCategory;
+  useEffect(() => {
+    getProducts().then((data) => {
+      setProducts(data);
+      setLoading(false);
     });
+  }, []);
 
-     return (
+  const categories = ["all", ...Array.from(new Set(products.map((p) => p.category)))];
+
+  const filtered = products.filter((p) => {
+    const matchesSearch = p.title.toLowerCase().includes(search.toLowerCase());
+    const matchesCategory = activeCategory === "all" || p.category === activeCategory;
+    return matchesSearch && matchesCategory;
+  });
+
+  if (loading) return <p>Loading products...</p>;
+
+  return (
     <div className={styles.layout}>
       <Sidebar
         categories={categories}
