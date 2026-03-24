0ded5acde89ab96002a59551db513eee308541b8
Ruslan <sergeikashtan1337@gmail.com>
2026-03-24 16:33:23 +0100

    tryisng to provide checkout

--- app/src/components/CheckOutModal/CheckOutModal.tsx
+++ app/src/components/CheckOutModal/CheckOutModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import styles from "./CheckoutModal.module.scss";
+import styles from "./CheckOutModal.module.scss";
 
 interface CartItem {
   productId: number;
