510b5560e0d3a5a1a16d6d944509f61d1b8b09cf
Ruslan <sergeikashtan1337@gmail.com>
2026-03-25 21:56:35 +0100

    testing checkout

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -2,7 +2,7 @@ import mongoose, { models } from "mongoose";
 
 export const OrdersSchema = new mongoose.Schema({
     items: {type: Array, required: true},
-    total: {type: Number, required: true},
+    total: {type: Number, required: false,default: 0},
     createdAt: { type: Date, default: Date.now }
 })
 
