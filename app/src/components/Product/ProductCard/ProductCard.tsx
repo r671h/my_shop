efc4d0865417bc70e22e75f75d32e83730f0557b
Ruslan <sergeikashtan1337@gmail.com>
2026-03-28 19:42:20 +0100

    fixing bugs

--- app/src/components/Product/ProductCard/ProductCard.tsx
+++ app/src/components/Product/ProductCard/ProductCard.tsx
@@ -3,9 +3,6 @@ import { Product } from "@/app/src/types";
 import styles from "./ProductCard.module.scss";
 import Link from "next/link";
 import { useCart } from "@/app/src/context/CartContext";
-import AuthModal from "../../AuthModal/AuthModal";
-import { useAuth } from "@/app/src/context/AuthConext";
-import { useState } from "react";
 
 type Props = {
   product: Product;
@@ -13,21 +10,9 @@ type Props = {
 
 export default function ProductCard({product} : Props) {
 
-  const { isLoggedIn } = useAuth();
-  const [showAuthPrompt, setShowAuthPrompt] = useState(false);
-
   const { addToCart, isInCart, updateQuantity, items } = useCart();
   const cartItem = items.find(i => i.productId === product!.id);
   const quantity = cartItem ? cartItem.quantity : 0;
-
-  const handleClick = (e : React.MouseEvent) => {
-    if (!isLoggedIn) {
-      e.preventDefault();
-      e.stopPropagation();
-      setShowAuthPrompt(true);
-    };
-    isInCart(product!.id) ? updateQuantity(product!.id, quantity+1) : addToCart(product!);
-  }
 
   return (
     
@@ -43,13 +28,9 @@ export default function ProductCard({product} : Props) {
         </Link>
         <button 
           className={styles.button}
-          onClick = {handleClick}
+          onClick = {() => isInCart(product!.id) ? updateQuantity(product!.id, quantity+1) : addToCart(product!)}
           >Add to Cart
         </button>
-        <AuthModal
-          isOpen={showAuthPrompt}
-          onClose={() => setShowAuthPrompt(false)}
-        />
       </div>
     
   );
