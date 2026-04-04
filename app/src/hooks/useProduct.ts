c9da3d6398846a8787301cd807a8f92965ec1f1b
Ruslan <sergeikashtan1337@gmail.com>
2026-04-04 20:55:01 +0200

    implementing SWR

--- app/src/hooks/useProduct.ts
+++ app/src/hooks/useProduct.ts
@@ -20,7 +20,7 @@ export function useProduct(id : string){
 
     return {
         product: data,
-        loading: isLoading,
+        loading: false,
         error: error?.message || null
     };
 }
