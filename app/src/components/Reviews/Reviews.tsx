4107792448c58613d5b97954edadacd71f9669c9
Ruslan <sergeikashtan1337@gmail.com>
2026-04-21 21:32:29 +0200

    providing reviews

--- app/src/components/Reviews/Reviews.tsx
+++ app/src/components/Reviews/Reviews.tsx
@@ -50,9 +50,6 @@ export default function Reviews({ productId }: Props) {
         headers: { Authorization: `Bearer ${token}` },
       });
       mutate();
-      console.log("review.userId:", reviews[0].userId);
-      console.log("user.id:", (user as any).id);
-      console.log("user._id:", (user as any)._id);
     } catch (e: any) {
       console.error("Error deleting review:", e.message);
     }
